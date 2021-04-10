db5b72d676f8ddc5a0ec7d1942fe962eaf89df24
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2021-04-10 15:03:45 +0200

    Revisit Converter

--- src/Converter.cpp
+++ src/Converter.cpp
@@ -26,8 +26,9 @@ namespace Converter {
 std::vector<cv::Mat> toDescriptorVector(const cv::Mat &Descriptors) {
   std::vector<cv::Mat> vDesc;
   vDesc.reserve(Descriptors.rows);
-  for(int j = 0; j < Descriptors.rows; j++)
+  for(int j = 0; j < Descriptors.rows; j++) {
     vDesc.push_back(Descriptors.row(j));
+  }
 
   return vDesc;
 }
@@ -37,45 +38,50 @@ g2o::SE3Quat toSE3Quat(const cv::Mat &cvT) {
   R << cvT.at<float>(0, 0), cvT.at<float>(0, 1), cvT.at<float>(0, 2), cvT.at<float>(1, 0), cvT.at<float>(1, 1),
     cvT.at<float>(1, 2), cvT.at<float>(2, 0), cvT.at<float>(2, 1), cvT.at<float>(2, 2);
 
-  Eigen::Matrix<double, 3, 1> t(cvT.at<float>(0, 3), cvT.at<float>(1, 3), cvT.at<float>(2, 3));
+  const Eigen::Matrix<double, 3, 1> t(cvT.at<float>(0, 3), cvT.at<float>(1, 3), cvT.at<float>(2, 3));
 
   return g2o::SE3Quat(R, t);
 }
 
 cv::Mat toCvMat(const g2o::SE3Quat &SE3) {
-  Eigen::Matrix<double, 4, 4> eigMat = SE3.to_homogeneous_matrix();
+  const Eigen::Matrix<double, 4, 4> eigMat = SE3.to_homogeneous_matrix();
   return toCvMat(eigMat);
 }
 
 cv::Mat toCvMat(const g2o::Sim3 &Sim3) {
-  Eigen::Matrix3d eigR = Sim3.rotation().toRotationMatrix();
-  Eigen::Vector3d eigt = Sim3.translation();
-  double s = Sim3.scale();
+  const Eigen::Matrix3d eigR = Sim3.rotation().toRotationMatrix();
+  const Eigen::Vector3d eigt = Sim3.translation();
+  const double s = Sim3.scale();
   return toCvSE3(s * eigR, eigt);
 }
 
 cv::Mat toCvMat(const Eigen::Matrix<double, 4, 4> &m) {
   cv::Mat cvMat(4, 4, CV_32F);
-  for(int i = 0; i < 4; i++)
-    for(int j = 0; j < 4; j++)
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
       cvMat.at<float>(i, j) = m(i, j);
+    }
+  }
 
   return cvMat.clone();
 }
 
 cv::Mat toCvMat(const Eigen::Matrix3d &m) {
   cv::Mat cvMat(3, 3, CV_32F);
-  for(int i = 0; i < 3; i++)
-    for(int j = 0; j < 3; j++)
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 3; j++) {
       cvMat.at<float>(i, j) = m(i, j);
+    }
+  }
 
   return cvMat.clone();
 }
 
 cv::Mat toCvMat(const Eigen::Matrix<double, 3, 1> &m) {
   cv::Mat cvMat(3, 1, CV_32F);
-  for(int i = 0; i < 3; i++)
+  for(int i = 0; i < 3; i++) {
     cvMat.at<float>(i) = m(i);
+  }
 
   return cvMat.clone();
 }
@@ -87,6 +93,7 @@ cv::Mat toCvSE3(const Eigen::Matrix<double, 3, 3> &R, const Eigen::Matrix<double
       cvMat.at<float>(i, j) = R(i, j);
     }
   }
+
   for(int i = 0; i < 3; i++) {
     cvMat.at<float>(i, 3) = t(i);
   }
@@ -118,16 +125,13 @@ Eigen::Matrix<double, 3, 3> toMatrix3d(const cv::Mat &cvMat3) {
 }
 
 std::vector<float> toQuaternion(const cv::Mat &M) {
-  Eigen::Matrix<double, 3, 3> eigMat = toMatrix3d(M);
-  Eigen::Quaterniond q(eigMat);
+  const Eigen::Matrix<double, 3, 3> eigMat = toMatrix3d(M);
+  const Eigen::Quaterniond q(eigMat);
 
-  std::vector<float> v(4);
-  v[0] = q.x();
-  v[1] = q.y();
-  v[2] = q.z();
-  v[3] = q.w();
-
-  return v;
+  return {static_cast<float>(q.x()),
+          static_cast<float>(q.y()),
+          static_cast<float>(q.z()),
+          static_cast<float>(q.w())};
 }
 
 }  // namespace Converter
