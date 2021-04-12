02c472c377724d776a8e11beeb2456a23e3edff9
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2021-04-12 15:33:51 +0200

    remove some warning from clang

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -18,6 +18,8 @@ target_compile_options(
   options
   INTERFACE
   $<$<CXX_COMPILER_ID:Clang>:-Weverything;-Wno-c++98-compat-pedantic;-fcolor-diagnostics;-Wno-unused-macros>
+  $<$<CXX_COMPILER_ID:Clang>:-Wno-padded;-Wno-float-equal;-Wno-sign-conversion;-Wno-double-promotion;-Wno-shorten-64-to-32>
+  $<$<CXX_COMPILER_ID:Clang>:-Wno-implicit-float-conversion;-Wno-float-conversion;-Wno-over-aligned>
   $<$<CXX_COMPILER_ID:GNU>:-Wall;-W;-Wpedantic;-Wshadow;-Wnon-virtual-dtor;-Wold-style-cast;-Wunused;-Wformat=2>
   $<$<CXX_COMPILER_ID:MSVC>:/W3;/permissive-;/bigobj>
 )
